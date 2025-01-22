70158d10e216e01675b98ad84d8954160bed7951
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2025-01-22 22:36:57 +0000

    .

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose.connect(DB, {}).then(() => console.log("DB connected successful"));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
